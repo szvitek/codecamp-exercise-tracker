c8dc24c43e456dd2099c4a9c42c53428ade2d29d
szvitek <gy.szvitek@gmail.com>
2024-02-24 01:18:01 +0000

    refactor(App): express-generator refactor + morgan,helmet,debug installed

--- app.js
+++ app.js
@@ -2,13 +2,16 @@ const createError = require('http-errors');
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const connectDB = require('./config/db');
+const logger = require('morgan');
+const helmet = require('helmet');
 const userRoutes = require('./routes/users');
 require('dotenv').config();
 
+app.use(logger('dev'));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
 app.use(express.static('public'));
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
@@ -43,13 +46,4 @@ app.use(function (err, req, res, next) {
   });
 });
 
-connectDB()
-  .then(() => {
-    const listener = app.listen(process.env.PORT || 3000, () => {
-      console.log('Your app is listening on port ' + listener.address().port);
-    });
-  })
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+module.exports = app;
